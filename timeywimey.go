7b78d883ebb40c5c8a61bd113b1c451ef075bc5a
Christof Horschitz <horschitz@gmail.com>
2016-03-15 21:10:27 +0100

    Update timeywimey.go

--- timeywimey.go
+++ timeywimey.go
@@ -5,6 +5,9 @@ import (
 	"time"
 )
 
+// People assume that time is a strict progression of cause to effect,
+// but actually, from a non-linear, non-subjective viewpoint,
+// it's more like a big ball of wibbly-wobbly... timey-wimey... stuff
 type timeywimey struct {
 	extectedWorkTime string
 	actualWorkTime   string
